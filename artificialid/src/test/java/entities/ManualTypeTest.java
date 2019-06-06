b6addc3be3cbcadadafc803b47322ceb93db12eb
AlekseyBykov <aleksey.n.bykov@gmail.com>
2019-06-06 13:26:39 +0400

    Added Surefire plugin for executing the unit tests. Added test for saving entity with the manual assigned identifier.

--- artificialid/src/test/java/entities/ManualTypeTest.java
+++ artificialid/src/test/java/entities/ManualTypeTest.java
@@ -1,13 +1,10 @@
 package entities;
 
-import org.hibernate.Session;
-import org.hibernate.SessionFactory;
-import org.hibernate.Transaction;
+import org.hibernate.*;
 import org.hibernate.boot.MetadataSources;
 import org.hibernate.boot.registry.*;
 import org.hibernate.id.IdentifierGenerationException;
-import org.testng.annotations.BeforeClass;
-import org.testng.annotations.Test;
+import org.testng.annotations.*;
 
 //import static org.testng.Assert.*;
 
@@ -26,11 +23,27 @@ public class ManualTypeTest {
     }
 
     @Test(expectedExceptions = IdentifierGenerationException.class)
-    public void test__shouldPass__SaveManualType() {
+    public void test__shouldPass__saveManualTypeWithoutId() {
         try(Session session = sessionFactory.openSession()) {
             Transaction tx = session.beginTransaction();
+
             ManualType manualType = new ManualType();
             manualType.setName("some name");
+
+            session.save(manualType);
+
+            tx.commit();
+        }
+    }
+
+    @Test
+    public void test__shouldPass__saveManualTypeWithId() {
+        try(Session session = sessionFactory.openSession()) {
+            Transaction tx = session.beginTransaction();
+
+            ManualType manualType = new ManualType();
+            manualType.setId(1L);
+            manualType.setName("some another name");
 
             session.save(manualType);
 
