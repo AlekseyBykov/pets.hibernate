88a8ded092e67ddf7faefa466db861f334163f6b
AlekseyBykov <aleksey.n.bykov@gmail.com>
2019-09-03 00:54:50 +0400

    Updated Hibernate version. Registered custom SQL function (customization for date_trunc()).

---
+++ core/src/main/java/alekseybykov/portfolio/hibernate/psql/functions/CustomSqlFunctions.java
@@ -0,0 +1,27 @@
+package alekseybykov.portfolio.hibernate.psql.functions;
+
+import org.hibernate.boot.MetadataBuilder;
+import org.hibernate.boot.spi.MetadataBuilderContributor;
+import org.hibernate.dialect.function.SQLFunctionTemplate;
+import org.hibernate.type.StandardBasicTypes;
+
+/**
+ * Registering with some customization of SQL function
+ * before using it in JPQL or Criteria API.
+ *
+ * @author  aleksey.n.bykov@gmail.com
+ * @version 1.0
+ * @since   2019-09-02
+ */
+public class CustomSqlFunctions implements MetadataBuilderContributor {
+    @Override
+    public void contribute(MetadataBuilder metadataBuilder) {
+        metadataBuilder.applySqlFunction(
+                "truncate_for_minutes",
+                new SQLFunctionTemplate(
+                        StandardBasicTypes.TIMESTAMP,
+                        "date_trunc('minute', (?1 AT TIME ZONE ?2))"
+                )
+        );
+    }
+}
