5878adba498ae3ca10b4129bd51bd47c7f248df8
AlekseyBykov <aleksey.n.bykov@gmail.com>
2019-11-09 17:09:49 +0400

    Added test suite for examples of obtaining entities.

---
+++ core/src/test/java/alekseybykov/portfolio/hibernate/session/ReadingEntitiesTest.java
@@ -0,0 +1,43 @@
+//
+// Feel free to use these solutions in your work.
+//
+package alekseybykov.portfolio.hibernate.session;
+
+import alekseybykov.portfolio.hibernate.TestContextHook;
+import alekseybykov.portfolio.hibernate.annotations.id.AutoIdentifiedEntity;
+import common.utils.SessionUtil;
+import org.hibernate.ObjectNotFoundException;
+import org.hibernate.Session;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+/**
+ * @author  aleksey.n.bykov@gmail.com
+ * @version 2019-11-09
+ */
+@ExtendWith({TestContextHook.class})
+@DisplayName("Tests for reading entities")
+class ReadingEntitiesTest {
+
+    @Test
+    @DisplayName("Get a non-existent entity by identifier")
+    void testGetMissingEntity() {
+        try (Session session = SessionUtil.getSession()) {
+            AutoIdentifiedEntity missingEntity = session.get(AutoIdentifiedEntity.class, 100L);
+            assertNull(missingEntity);
+        }
+    }
+
+    @Test
+    @DisplayName("Load a non-existent entity by identifier through proxy")
+    void testLoadMissingEntityThroughProxy() {
+        try (Session session = SessionUtil.getSession()) {
+            assertThrows(ObjectNotFoundException.class,
+                    () -> session.load(AutoIdentifiedEntity.class, 100L));
+        }
+    }
+}
